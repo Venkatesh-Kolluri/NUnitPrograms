7d0200026fd08a6727c3af6549aa36a53f7de475
Venkatesh-Kolluri <118035002+Venkatesh-Kolluri@users.noreply.github.com>
2023-04-09 15:16:16 +0530

    Add files via upload

---
+++ Payment.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitProgramming
+{
+    internal class Payment
+    {
+      /*  public static void Main(string[] args)
+        {
+            double P;
+            Console.WriteLine("Enter the Principal Amount: ");
+            P =Convert.ToDouble(Console.ReadLine());
+
+            double Y;
+            Console.WriteLine("Enter No.of Years: ");
+            Y =Convert.ToDouble(Console.ReadLine());
+
+            double R;
+            Console.WriteLine("Enter the rate of interest: ");
+            R = Convert.ToDouble(Console.ReadLine());
+
+            double result=monthlyPayment(P,Y,R);
+            Console.WriteLine("The Monthly Payment amount is: "+result);
+
+            
+        }*/
+        public static double monthlyPayment(double P,double Y,double R)
+        {
+            double n = 12 * Y ;
+            double r = R / (12 * 100);
+
+            double pay = (P * r) / 1-Math.Pow( (1 + r), -n);
+            return pay;
+        }
+    }
+}
---
+++ Temperature.cs
@@ -0,0 +1,42 @@
+﻿namespace NUnitProgramming
+{
+    internal class Temperature
+    {
+      /*  static void Main(string[] args)
+        {
+            int c;
+            Console.WriteLine("Enter the degree celicius value: ");
+            c = Convert.ToInt32(Console.ReadLine());
+            if (c == 0)
+            {
+                int f;
+                Console.WriteLine("Enter the Fahrenheit Value: ");
+                f = Convert.ToInt32(Console.ReadLine());
+                int ans=fahrenheitConversion(f);
+                Console.WriteLine("The value in degree is: "+ans);
+                
+            }
+            else
+            {
+                int result = degreeConversion(c);
+
+                Console.WriteLine("The value in fehrenheit is: " + result);
+
+            }
+           
+        }*/
+        public static int degreeConversion(int c)
+        {
+            int fahrenheit =( (c * 9) / 5) + 32;
+            return fahrenheit;
+        }
+        public static int fahrenheitConversion(int f)
+        {
+            int degree = (f - 32) * 5 / 9;
+            return degree;
+        }
+
+      //  (c * (9/5))+32=f; c to f
+        //(F - 32) * 5/9=c; f to c
+    }
+}
